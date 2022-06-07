8f5b430d71d2f70d87f30befe68b05ab0cea6f1c
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-06-06 22:44:00 -0400

    wc: work in progress

--- breadsauce/src/App.tsx
+++ breadsauce/src/App.tsx
@@ -1,10 +1,59 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './index.css'
-
 function App() {
   return (
-    <main className='text-3xl font-bold'>
-      Hello world!
-    </main>
+    <header className='grid place-content-center border border-black text-center max-h-screen'>
+      <Parallax pages={4}>
+        <ParallaxLayer
+          className='grid place-content-center'
+          offset={0}
+          speed={1}
+        >
+          <h1 className='text-3xl font-bold'>
+            I want love.
+          </h1>
+        </ParallaxLayer>
+        <ParallaxLayer
+          className='grid place-content-center'
+          offset={.04}
+          speed={.5}
+        //sticky={{ start: .1 , end: 1 }}
+        >
+          <h1 className='text-3xl font-bold'>
+            I want the honeymoon-type of love,
+          </h1>
+        </ParallaxLayer>
+        <ParallaxLayer
+          className='grid place-content-center'
+          offset={.08}
+          speed={.3}
+        //sticky={{ start: 1, end: 1.5 }}
+        >
+          <h1 className='text-3xl font-bold'>
+            where butterflies still swarm on my stomach
+          </h1>
+        </ParallaxLayer>
+        <ParallaxLayer
+          className='grid place-content-center'
+          offset={.12}
+          speed={.1}
+        >
+          <h1 className='text-3xl font-bold'>
+            and a hint of hesitation stumbles from your lips
+          </h1>
+        </ParallaxLayer>
+        <ParallaxLayer
+          className='grid place-content-center'
+          //offset={.6}
+          //speed={.05}
+          sticky={{ start: 1, end: 1.5 }}
+        >
+          <h1 className='text-3xl font-bold'>
+            on mine.
+          </h1>
+        </ParallaxLayer>
+      </Parallax>
+    </header >
   )
 }
 
