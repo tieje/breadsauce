2e5ea0245200680f3f66abbe60778c8ab35aa869
Thomas Francis <thomasfrancis@Thomass-MacBook-Air.local>
2022-06-10 20:50:36 -0400

    I fixed my kokoro.

--- breadsauce/src/ClosedLetter.tsx
+++ breadsauce/src/ClosedLetter.tsx
@@ -4,13 +4,13 @@ import HEARTSU from "./assets/images/heart-svgrepo-com.svg"
 import { ENVELOPE_CLOSED, ENVELOPE_OPEN_BOTTOM_FLAP_LEFT, ENVELOPE_OPEN_BOTTOM_FLAP_RIGHT, ENVELOPE_OPEN_BOTTOM_OPENING, ENVELOPE_TOP } from "./State/Envelope"
 
 const ClosedLetter = () => {
-    const imgStyleBase = 'absolute z-10 lg:ml-[12%] lg:mt-[10%] md:ml-[25%] md:mt-[21%] ml-[57%] mt-[48%] '
+    const imgStyleBase = 'xl:ml-[17%] xl:mt-[13%] lg:ml-[17%] lg:mt-[17%] absolute z-10 md:ml-[25%] md:mt-[21%] ml-[57%] mt-[48%] '
     const noBeat = imgStyleBase + 'motion-safe:animate-pulse'
     const Beat = imgStyleBase + 'motion-safe:animate-ping'
     const [beating, setBeating] = useState(false)
     return (
         <header
-            className='relative h-screen w-screen border border-black grid place-content-center bg-[url("./assets/images/white-roses.jpeg")] bg-cover'
+            className='relative h-screen w-screen grid place-content-center bg-[url("./assets/images/white-roses.jpeg")] bg-cover'
         >
             <a
                 href='#info'
@@ -44,7 +44,7 @@ const ClosedLetter = () => {
                 </animated.svg>
             </a>
             <a
-                className='absolute text-4xl z-0 lg:ml-[46.5%] lg:mt-[33%] md:ml-[43%] md:mt-[90%] ml-[35%] mt-[140%] hover:underline underline-offset-8 motion-safe:animate-bounce'
+                className='absolute text-4xl z-0 lg:ml-[46.5%] lg:mt-[40%] md:ml-[43%] md:mt-[90%] ml-[35%] mt-[140%] hover:underline underline-offset-8 motion-safe:animate-bounce'
                 onMouseOver={() => setBeating(true)}
                 href='#info'
             >
